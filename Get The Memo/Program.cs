a23a37d7c0fc0e2ff8c785781f1f35ab69403aa7
Dozerhank <65521931+Dozerhank@users.noreply.github.com>
2021-11-01 09:31:21 -0500

    Add files via upload

---
+++ Get The Memo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Get_The_Memo
+{
+    class Program
+    {
+        int[] valueArray;
+        public static int[] memo(int[] array, int x)
+        {
+            if (array[x] == 1 || array[x] == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return memo(array[], x + 1) + memo(array[], x + 1);
+            }
+            return array;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter the value of n: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int[] valueArray = new int[userInput];
+            Console.Write("The {0}th Fibonacci number is {1}", userInput, memo(valueArray, 0));
+        }
+    }
+}
